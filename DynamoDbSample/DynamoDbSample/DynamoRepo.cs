346eadcb8e36ad61884c08942fd1dd8b70c930c9
Emanuel Ramos <eramos@devexperts.com>
2021-11-25 11:10:13 +0000

    wait until table is active

--- DynamoDbSample/DynamoDbSample/DynamoRepo.cs
+++ DynamoDbSample/DynamoDbSample/DynamoRepo.cs
@@ -32,6 +32,7 @@ namespace DynamoDbSample
 
 			var json = item.ToJson();
 
+			// HERE'S THE Empty String issue
 			Console.WriteLine($"Retrieved Json {json}. This should come with an empty string");
 
 			return JsonSerializer.Deserialize<TDbEntity>(json, JsonSerialization.Settings);
@@ -41,6 +42,7 @@ namespace DynamoDbSample
 		{
 			var jsonText = JsonSerializer.Serialize(entity, JsonSerialization.Settings);
 
+			// HERE we can verify that the json includes the emptyString
 			Console.WriteLine($"Stored Json{jsonText}");
 
 			var item = Document.FromJson(jsonText);
@@ -74,11 +76,42 @@ namespace DynamoDbSample
 							},
 							BillingMode = BillingMode.PAY_PER_REQUEST
 						});
+
+				await WaitUntilTableReady(dynamoDb, tableName);
 			}
 			catch (ResourceInUseException)
 			{
 				Console.WriteLine($"Table {tableName} already exists");
 			}
+		}
+
+		private static async Task WaitUntilTableReady(IAmazonDynamoDB client, string tableName)
+		{
+			string status = null;
+			// Let us wait until table is created. Call DescribeTable.
+			do
+			{
+				Console.WriteLine($"Waiting for table {tableName} to be active");
+
+				await Task.Delay(5000);// Wait 5 seconds.
+
+				try
+				{
+					var res = await client.DescribeTableAsync(new DescribeTableRequest
+					{
+						TableName = tableName
+					});
+					Console.WriteLine("Table name: {0}, status: {1}",
+									res.Table.TableName,
+									res.Table.TableStatus);
+					status = res.Table.TableStatus;
+				}
+				catch (ResourceNotFoundException)
+				{
+					// DescribeTable is eventually consistent. So you might
+					// get resource not found. So we handle the potential exception.
+				}
+			} while (status != TableStatus.ACTIVE);
 		}
 	}
 }
